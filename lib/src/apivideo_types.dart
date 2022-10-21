f594ab04587b163c44e5fb363b417e1f88df4374
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2022-10-21 14:55:55 +0200

    fix(lib): add serializer and deserializer for types

--- lib/src/apivideo_types.dart
+++ lib/src/apivideo_types.dart
@@ -15,4 +15,11 @@ class VideoOptions {
   VideoType videoType;
 
   VideoOptions({required this.videoId, this.videoType = VideoType.vod});
+
+  /// Creates a [VideoOptions] from a [json] map.
+  factory VideoOptions.fromJson(Map<String, dynamic> json) =>
+      _$VideoOptionsFromJson(json);
+
+  /// Creates a json map from a [VideoOptions].
+  Map<String, dynamic> toJson() => _$VideoOptionsToJson(this);
 }
