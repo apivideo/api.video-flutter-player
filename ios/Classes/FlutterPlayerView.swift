098fb3692630c648ae8e5b1f3cdc60fddcc99902
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2022-10-24 17:59:30 +0200

    feat(ios): add ios player

---
+++ ios/Classes/FlutterPlayerView.swift
@@ -0,0 +1,101 @@
+import ApiVideoPlayer
+import AVFoundation
+import AVKit
+import Foundation
+
+class FlutterPlayerView: NSObject {
+    private let playerTexture = PlayerFlutterTexture()
+    private let textureRegistry: FlutterTextureRegistry
+    var textureId: Int64!
+    private let frameUpdater: FrameUpdater
+    private var displayLink: CADisplayLink!
+    private let playerController: ApiVideoPlayerController
+    private let playerLayer = AVPlayerLayer() // Only use to fix bugs according to flutter video_player plugin
+
+    private let events = PlayerEvents()
+
+    init(frame _: CGRect,
+         binaryMessenger _: FlutterBinaryMessenger,
+         textureRegistry: FlutterTextureRegistry,
+         videoId: String,
+         videoType: VideoType)
+    {
+        self.textureRegistry = textureRegistry
+        playerController = ApiVideoPlayerController(videoId: videoId, videoType: videoType, playerLayer: playerLayer, events: events)
+        textureId = self.textureRegistry.register(playerTexture)
+        frameUpdater = FrameUpdater(textureRegistry: self.textureRegistry, textureId: textureId)
+        displayLink = FlutterPlayerView.createDisplayLink(frameUpdater: frameUpdater)
+        super.init()
+        events.didPrepare = {
+            self.playerController.addOutput(output: self.playerTexture.videoOutput)
+            // Hack to load the first image. We don't need it in case of autoplay
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.textureRegistry.textureFrameAvailable(self.textureId)
+            }
+        }
+    }
+
+    static func createVideoOutput() -> AVPlayerItemVideoOutput {
+        let pixBuffAttributes = [kCVPixelBufferPixelFormatTypeKey: Int(kCVPixelFormatType_32BGRA),
+                                 kCVPixelBufferIOSurfacePropertiesKey: [:]] as [String: Any]
+        return AVPlayerItemVideoOutput(pixelBufferAttributes: pixBuffAttributes)
+    }
+
+    static func createDisplayLink(frameUpdater: FrameUpdater) -> CADisplayLink {
+        let displayLink = CADisplayLink(target: frameUpdater, selector: #selector(FrameUpdater.onDisplayLink(_:)))
+        displayLink.add(to: RunLoop.current, forMode: RunLoop.Mode.common)
+        displayLink.isPaused = true
+        return displayLink
+    }
+
+    func play() {
+        playerController.play()
+        displayLink.isPaused = false
+    }
+
+    func pause() {
+        playerController.pause()
+        displayLink.isPaused = true
+    }
+
+    func onTextureUnregistered() {
+        dispose()
+    }
+
+    func dispose() {
+        pause()
+        playerController.removeOutput(output: playerTexture.videoOutput)
+        displayLink.invalidate()
+        textureRegistry.unregisterTexture(textureId)
+    }
+}
+
+class FrameUpdater: NSObject {
+    private let textureRegistry: FlutterTextureRegistry
+    private let textureId: Int64
+
+    init(textureRegistry: FlutterTextureRegistry, textureId: Int64) {
+        self.textureRegistry = textureRegistry
+        self.textureId = textureId
+    }
+
+    @objc func onDisplayLink(_: CADisplayLink) {
+        textureRegistry.textureFrameAvailable(textureId)
+    }
+}
+
+class PlayerFlutterTexture: NSObject, FlutterTexture {
+    let videoOutput = FlutterPlayerView.createVideoOutput()
+
+    func copyPixelBuffer() -> Unmanaged<CVPixelBuffer>? {
+        let outputItemTime = videoOutput.itemTime(forHostTime: CACurrentMediaTime())
+        if videoOutput.hasNewPixelBuffer(forItemTime: outputItemTime) {
+            guard let pixelBuffer = videoOutput.copyPixelBuffer(forItemTime: outputItemTime, itemTimeForDisplay: nil) else {
+                return nil
+            }
+            return Unmanaged<CVPixelBuffer>.passRetained(pixelBuffer)
+        } else {
+            return nil
+        }
+    }
+}
--- ios/Classes/SwiftApiVideoPlayerPlugin.swift
+++ ios/Classes/SwiftApiVideoPlayerPlugin.swift
@@ -2,8 +2,22 @@ import Flutter
 import UIKit
 
 public class SwiftApiVideoPlayerPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-      let factory = FlutterApiVideoPlayerViewFactory(binaryMessenger: registrar.messenger())
-            registrar.register(factory, withId: "plugins.video.api/flutter_player_view")
-  }
+    private var textureRegistry: FlutterTextureRegistry!
+    private var messenger: FlutterBinaryMessenger!
+    private var api: MethodCallHandler!
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftApiVideoPlayerPlugin(registrar: registrar)
+        registrar.publish(instance)
+    }
+
+    init(registrar: FlutterPluginRegistrar) {
+        textureRegistry = registrar.textures()
+        messenger = registrar.messenger()
+        api = MethodCallHandler(binaryMessenger: messenger, controller: FlutterPlayerController(binaryMessenger: messenger, textureRegistry: textureRegistry))
+    }
+
+    public func detachFromEngine(for _: FlutterPluginRegistrar) {
+        api.dispose()
+    }
 }
