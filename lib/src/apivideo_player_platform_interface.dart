e11f401539fde0d30da9dc1c26a5cebfb4b1632f
MarDi66 <yohann.mtfpro@gmail.com>
2022-10-25 10:50:04 +0200

    feat(platform interface): Add isPlaying + currentTime + duration + seek

--- lib/src/apivideo_player_platform_interface.dart
+++ lib/src/apivideo_player_platform_interface.dart
@@ -10,6 +10,9 @@ abstract class ApiVideoPlayerPlatform extends PlatformInterface {
   static final Object _token = Object();
 
   static ApiVideoPlayerPlatform _instance = _PlatformImplementation();
+  bool isPlaying = false;
+  Duration currentTime = const Duration();
+  Duration duration = const Duration();
 
   /// The default instance of [ApiVideoPlayerPlatform] to use.
   ///
@@ -34,14 +37,22 @@ abstract class ApiVideoPlayerPlatform extends PlatformInterface {
     throw UnimplementedError('dispose() has not been implemented.');
   }
 
+  /// Starts the video playback.
   Future<void> play(int textureId) {
     throw UnimplementedError('play() has not been implemented.');
   }
 
+  /// Stops the video playback.
   Future<void> pause(int textureId) {
     throw UnimplementedError('pause() has not been implemented.');
   }
 
+  /// Sets the video position to a [Duration] from the start.
+  Future<void> seek(int textureId, Duration position) {
+    throw UnimplementedError('seek() has not been implemented.');
+  }
+
+  /// Returns a widget displaying the video with a given textureID.
   Widget buildView(int textureId) {
     throw UnimplementedError('buildView() has not been implemented.');
   }
