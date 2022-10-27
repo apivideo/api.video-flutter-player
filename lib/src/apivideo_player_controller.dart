204871b52d694d8ebeeb82d87d9c711d6d6bbd17
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2022-10-27 10:00:16 +0200

    feat(player): add a way to return events from the native player

--- lib/src/apivideo_player_controller.dart
+++ lib/src/apivideo_player_controller.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:apivideo_player/apivideo_player.dart';
 import 'package:apivideo_player/src/apivideo_types.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 import 'apivideo_player_platform_interface.dart';
 
@@ -13,11 +17,36 @@ class ApiVideoPlayerController {
   static const int kUninitializedTextureId = -1;
   int _textureId = kUninitializedTextureId;
 
+  StreamSubscription<dynamic>? _eventSubscription;
+  List<ApiVideoPlayerControllerListener> listeners = [];
+
   /// This is just exposed for testing. Do not use it.
   @visibleForTesting
   int get textureId => _textureId;
 
-  ApiVideoPlayerController(this.videoOptions);
+  ApiVideoPlayerController({
+    required this.videoOptions,
+    VoidCallback? onReady,
+    VoidCallback? onPlay,
+    VoidCallback? onPause,
+    VoidCallback? onEnd,
+    Function(Object)? onError,
+  }) {
+    listeners.add(ApiVideoPlayerControllerListener(
+        onReady: onReady,
+        onPlay: onPlay,
+        onPause: onPause,
+        onEnd: onEnd,
+        onError: onError));
+  }
+
+  ApiVideoPlayerController.fromListener(
+      {required this.videoOptions,
+      ApiVideoPlayerControllerListener? listener}) {
+    if (listener != null) {
+      listeners.add(listener);
+    }
+  }
 
   Future<bool> get isPlaying async {
     return _playerPlatform.isPlaying(_textureId);
@@ -39,8 +68,13 @@ class ApiVideoPlayerController {
   }
 
   Future<void> initialize() async {
-    _textureId =
-        await _playerPlatform.create(videoOptions) ?? kUninitializedTextureId;
+    _textureId = await _playerPlatform.initialize() ?? kUninitializedTextureId;
+
+    _eventSubscription = _playerPlatform
+        .playerEventsFor(_textureId)
+        .listen(_eventListener, onError: _errorListener);
+
+    await _playerPlatform.create(_textureId, videoOptions);
 
     return;
   }
@@ -54,10 +88,77 @@ class ApiVideoPlayerController {
   }
 
   Future<void> dispose() async {
-    return _playerPlatform.dispose(_textureId);
+    await _eventSubscription?.cancel();
+    listeners.clear();
+    await _playerPlatform.dispose(_textureId);
+    return;
   }
 
   Future<void> seek(Duration offset) async {
     return _playerPlatform.seek(_textureId, offset.inMilliseconds);
   }
+
+  void addListener(ApiVideoPlayerControllerListener listener) {
+    listeners.add(listener);
+  }
+
+  void removeListener(ApiVideoPlayerControllerListener listener) {
+    listeners.remove(listener);
+  }
+
+  void _errorListener(Object obj) {
+    final PlatformException e = obj as PlatformException;
+    for (var listener in [...listeners]) {
+      if (listener.onError != null) {
+        listener.onError!(e);
+      }
+    }
+  }
+
+  void _eventListener(PlayerEvent event) {
+    switch (event.type) {
+      case PlayerEventType.ready:
+        for (var listener in [...listeners]) {
+          if (listener.onReady != null) {
+            listener.onReady!();
+          }
+        }
+        break;
+      case PlayerEventType.played:
+        for (var listener in [...listeners]) {
+          if (listener.onPlay != null) {
+            listener.onPlay!();
+          }
+        }
+        break;
+      case PlayerEventType.paused:
+        for (var listener in [...listeners]) {
+          if (listener.onPause != null) {
+            listener.onPause!();
+          }
+        }
+        break;
+      case PlayerEventType.ended:
+        for (var listener in [...listeners]) {
+          if (listener.onEnd != null) {
+            listener.onEnd!();
+          }
+        }
+        break;
+      case PlayerEventType.unknown:
+        // Nothing to do
+        break;
+    }
+  }
+}
+
+class ApiVideoPlayerControllerListener {
+  final VoidCallback? onReady;
+  final VoidCallback? onPlay;
+  final VoidCallback? onPause;
+  final VoidCallback? onEnd;
+  final Function(Object)? onError;
+
+  ApiVideoPlayerControllerListener(
+      {this.onReady, this.onPlay, this.onPause, this.onEnd, this.onError});
 }
