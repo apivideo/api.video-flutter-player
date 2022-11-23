04fb55ac21f830c436df78b72af2f5e24939d334
Romain Petit <contact.romain.petit@gmail.com>
2022-11-23 14:14:41 +0100

    feat() add PlayerLifeCycleObserver

---
+++ lib/src/apivideo_player_life_cycle_observer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+import '../apivideo_player.dart';
+
+class PlayerLifeCycleObserver extends Object with WidgetsBindingObserver {
+  final ApiVideoPlayerController controller;
+  bool _wasPlayingBeforePause = false;
+
+  PlayerLifeCycleObserver(this.controller);
+
+  void initialize() {
+    _ambiguate(WidgetsBinding.instance)!.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        if (_wasPlayingBeforePause) {
+          controller.play();
+        }
+        break;
+      case AppLifecycleState.paused:
+        pause();
+        break;
+    }
+  }
+
+  void pause() async {
+    _wasPlayingBeforePause = await controller.isPlaying;
+    controller.pause();
+  }
+
+  void dispose() {
+    _ambiguate(WidgetsBinding.instance)!.removeObserver(this);
+  }
+}
+
+T? _ambiguate<T>(T? value) => value;
