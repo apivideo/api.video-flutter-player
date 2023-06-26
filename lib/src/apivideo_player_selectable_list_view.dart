a800db7266120e49996f5b9bfc3a2e2815545510
Romain Petit <contact.romain.petit@gmail.com>
2023-06-26 14:24:51 +0200

    chore() make widget stateless, rename 'selectedElement' to 'selectedElement'

--- lib/src/apivideo_player_selectable_list_view.dart
+++ lib/src/apivideo_player_selectable_list_view.dart
@@ -1,57 +1,37 @@
 import 'package:apivideo_player/apivideo_player.dart';
 import 'package:flutter/material.dart';
 
-class ApiVideoPlayerSelectableListView extends StatefulWidget {
+class ApiVideoPlayerSelectableListView extends StatelessWidget {
   const ApiVideoPlayerSelectableListView({
     super.key,
     required this.items,
-    required this.selectedElement,
+    required this.selectedItem,
     required this.onSelected,
     required this.theme,
   });
 
   final List<Object> items;
-  final Object selectedElement;
+  final Object selectedItem;
   final ValueChanged<Object> onSelected;
   final PlayerTheme theme;
 
   @override
-  State<ApiVideoPlayerSelectableListView> createState() =>
-      _ApiVideoPlayerSelectableListViewState();
-}
-
-class _ApiVideoPlayerSelectableListViewState
-    extends State<ApiVideoPlayerSelectableListView> {
-  _ApiVideoPlayerSelectableListViewState();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: widget.items.length,
+      itemCount: items.length,
       itemBuilder: (BuildContext context, int index) {
-        final item = widget.items[index];
-        bool isSelected = item == widget.selectedElement;
+        final item = items[index];
+        bool isSelected = item == selectedItem;
         return ListTile(
           title: Text('$item'),
           trailing: isSelected
               ? Icon(
                   Icons.check,
-                  color: widget.theme.selectedSpeedRateColor,
+                  color: theme.checkIconColor,
                 )
               : null,
           onTap: () {
-            isSelected = true;
-            widget.onSelected(item);
+            onSelected(item);
           },
         );
       },
