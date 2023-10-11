07aa52931b3f815d15e5a3f2eab9435a6c0d003c
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2023-10-11 10:20:03 +0200

    feat(lib): make player video public

--- lib/apivideo_player.dart
+++ lib/apivideo_player.dart
@@ -3,9 +3,9 @@ export 'src/apivideo_types.dart';
 export 'src/platform/apivideo_mobile_player_platform.dart';
 export 'src/style/apivideo_style.dart';
 export 'src/widgets/apivideo_player.dart';
-
 export 'src/widgets/apivideo_player_controls_bar.dart';
+export 'src/widgets/apivideo_player_opacity.dart';
 export 'src/widgets/apivideo_player_overlay.dart';
 export 'src/widgets/apivideo_player_settings_bar.dart';
 export 'src/widgets/apivideo_player_time_slider.dart';
-export 'src/widgets/apivideo_player_opacity.dart';
+export 'src/widgets/apivideo_player_video.dart';
